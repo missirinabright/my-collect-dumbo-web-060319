d2cb8af1ab3cecf2f5c9a63ab7ec5787fcabe3fd
Irina Bright <missirinabright@gmail.com>
2019-05-28 19:27:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array)
   #starting with the first evelemt
   i = 0
-  
+  #can handle an empty collection
   collect = []
+  #establishing the limit
   while i < array.length
+  #shoveling 
     collect << yield(array[i])
     i+=1
   end
