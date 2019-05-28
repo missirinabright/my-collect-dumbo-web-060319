2689e450e53942851e81c21f713593a2d4c82128
Irina Bright <missirinabright@gmail.com>
2019-05-28 19:31:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  #starting with the first evelemt
+  i = 0
+  #can handle an empty collection
+  collect = []
+  #establishing the limit
+  while i < array.length
+  #shoveling to yield the correct element from a given collection
+    collect << yield(array[i])
+     i+=1
+  end
+  collect
+end
 
 
